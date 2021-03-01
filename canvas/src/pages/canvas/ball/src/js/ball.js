d5796d566e5981616707af2fe96259758ba2829f
wangcuijuan <1508541248@qq.com>
2021-03-01 23:05:17 +0800

    ball

---
+++ canvas/src/pages/canvas/ball/src/js/ball.js
@@ -0,0 +1,14 @@
+class ball{
+    constructor(location,r,id,name,speed,angle,color){
+        this.location=location
+        this.r=r
+        this.id=id
+        this.name=name
+        this.speed=speed
+        this.angle=angle
+        this.color=color
+    }
+    move(time){
+
+    }
+}
